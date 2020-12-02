b36e2a35a2fc07a6af9b470cb23de63f3fd636ea
Neel Shah <neelshah.sa@gmail.com>
2020-12-02 16:37:22 +0100

    Day 2 ruby

---
+++ ruby/02.rb
@@ -0,0 +1,27 @@
+class Password
+  def initialize(s)
+    policy, @pass = s.split(': ')
+    range, @char = policy.split(' ')
+    @min, @max = range.split('-').map(&:to_i)
+  end
+
+  def valid1?
+    @pass.count(@char).between?(@min, @max)
+  end
+
+  # ^ = xor
+  def valid2?
+    !!(@pass[@min - 1] == @char) ^ !!(@pass[@max - 1] == @char)
+  end
+end
+
+input = File.readlines("#{__dir__}/../files/02.input").map(&:strip)
+passwords = input.map { |s| Password.new(s) }
+
+puts '='*20
+puts 'part 1'
+puts passwords.count(&:valid1?)
+
+puts '='*20
+puts 'part 2'
+puts passwords.count(&:valid2?)
