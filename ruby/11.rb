62c2657059ef13af7ef82228911c19d9508ab6af
Neel Shah <neelshah.sa@gmail.com>
2021-01-09 18:25:21 +0100

    Day 11 ruby part 2

--- ruby/11.rb
+++ ruby/11.rb
@@ -1,43 +1,46 @@
-# input = "L.LL.LL.LL
-# LLLLLLL.LL
-# L.L.L..L..
-# LLLL.LL.LL
-# L.LL.LL.LL
-# L.LLLLL.LL
-# ..L.L.....
-# LLLLLLLLLL
-# L.LLLLLL.L
-# L.LLLLL.LL".lines.map(&:strip).map(&:chars)
-
-require 'pry'
-
-
 class Grid
   attr_accessor :grid
+
+  DIRECTIONS = [-1, 0, 1].product([-1, 0, 1]) - [[0, 0]]
 
   def initialize(grid)
     @grid = grid
   end
 
-  def neighbours(i, j)
-    indexes = [i - 1, i, i + 1].product([j - 1, j, j + 1]) - [[i, j]]
-    indexes = indexes.select { |x, y| x >= 0 && x < grid.length && y >= 0 && y < grid.first.length }
-    indexes.map { |x, y| grid[x][y] }
+  def ==(g)
+    g.class == self.class && g.grid == self.grid
   end
 
-  def occupied_count(i, j)
-    occupied_count = neighbours(i, j).count('#')
+  def in_grid?(x, y)
+    x >= 0 && x < grid.length && y >= 0 && y < grid.first.length
   end
 
-  def transform
+  def fetch(x, y)
+    return nil unless in_grid?(x, y)
+    grid[x][y]
+  end
+
+  def occupied_neighbours(i, j)
+    DIRECTIONS.count { |x, y| fetch(i + x, j + y) == '#' }
+  end
+
+  def occupied_visible(i, j)
+    DIRECTIONS.count do |x, y|
+      x2, y2 = i + x, j + y
+      x2, y2 = x2 + x, y2 + y while fetch(x2, y2) == '.'
+      fetch(x2, y2) == '#'
+    end
+  end
+
+  def transform(scope:, threshold:)
     Grid.new(
       grid.map.with_index do |row, i|
         row.map.with_index do |cell, j|
-          occupied_count = occupied_count(i, j)
+          occupied_count = send("occupied_#{scope}", i, j)
 
           case cell
           when '.' then '.'
-          when '#' then occupied_count >= 4 ? 'L' : '#'
+          when '#' then occupied_count >= threshold ? 'L' : '#'
           when 'L' then occupied_count == 0 ? '#' : 'L'
           end
         end
@@ -45,15 +48,13 @@ class Grid
     )
   end
 
-  def ==(g)
-    g.class == self.class && g.grid == self.grid
-  end
+  def self.steady_state_oc(input, part: 1)
+    opts = part == 1 ? { scope: :neighbours, threshold: 4 } : { scope: :visible, threshold: 5 }
 
-  def self.steady_state_oc(input)
     g = Grid.new(input)
-    g2 = g.transform
+    g2 = g.transform(**opts)
 
-    g, g2 = g2, g2.transform until g2 == g
+    g, g2 = g2, g2.transform(opts) until g2 == g
     g.grid.flatten.count('#')
   end
 end
@@ -66,3 +67,4 @@ puts Grid.steady_state_oc(input)
 
 puts '='*20
 puts 'part 2'
+puts Grid.steady_state_oc(input, part: 2)
